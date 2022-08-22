7aa19b25636400c65e5d6e2d1ee7b414387adc72
Ajay-2007 <iit2017040@iiita.ac.in>
2022-08-22 14:43:31 +0530

    created category model

--- online_restaurant_main/settings.py
+++ online_restaurant_main/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'category',
 ]
 
 MIDDLEWARE = [
@@ -119,6 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = [
+    'online_restaurant_main/static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
